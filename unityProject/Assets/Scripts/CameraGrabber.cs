2506196c31e15946fc5d30b6ec529a204cb88b56
manumanmax <manua380@hotmail.fr>
2015-04-02 12:30:52 +0200

    moving cursor color for merge in cameraGrabber

--- unityProject/Assets/Scripts/CameraGrabber.cs
+++ unityProject/Assets/Scripts/CameraGrabber.cs
@@ -6,20 +6,33 @@ public class CameraGrabber : MonoBehaviour {
     private Ray ray;
     private RaycastHit hit;
     private CursorColor cursCol;
-    
+    private static Texture2D t;
+    public static int size = 6;
+    public Vector2 cursorPos;
+
+    public bool display = false;
+    public bool dragOk = false;
+
      
 	// Use this for initialization
 	void Start () {
 	
 	}
+    private void Awake()
+    {
+        t = new Texture2D(size, size, TextureFormat.RGB24, true);
+        t.name = "Procedural Texture";
+        FillTexture(Color.green);
+        cursorPos = new Vector2(Screen.width / 2, Screen.height / 2 - 10);
+    }
 
    
 
 	// Update is called once per frame
 	void Update () {
 
-        ray = Camera.main.ScreenPointToRay(new Vector3(cursCol.cursorPos.x, cursCol.cursorPos.y, 0));
-
+        ray = Camera.main.ScreenPointToRay(new Vector3(0.5f,0.5f,0.0f));
+        Debug.Log(ray);
         if (cursCol == null)
         {
             if (Physics.Raycast(ray, out hit))
@@ -29,22 +42,26 @@ public class CameraGrabber : MonoBehaviour {
 
                 if (cursCol != null)
                 {
-                   
-                    Debug.Log("cursorColor found : " + cursCol.dragOk);
-                    if (cursCol.dragOk)
+                    display = true;
+                    Debug.Log("cursorColor found : " + dragOk);
+                    if (dragOk)
                     {
                         Vector3 worldPos = Camera.main.ScreenToWorldPoint(
-                            new Vector3(cursCol.cursorPos.x, cursCol.cursorPos.y, hit.distance));
+                            new Vector3(cursorPos.x, cursorPos.y, hit.distance));
                         worldPos = new Vector3(worldPos.x, worldPos.y, hitObject.transform.position.z);
                         hitObject.rigidbody.MovePosition(worldPos);
                         Debug.Log("dragging");
                     }
                 }
+                else
+                {
+                    display = false;
+                }
             }
         }
         else
         {
-            if (cursCol.dragOk)
+            if (dragOk)
             {
                 Vector3 worldPos = Camera.main.ScreenToWorldPoint(
                     new Vector3(Input.mousePosition.x, Input.mousePosition.y, hit.distance));
@@ -60,4 +77,32 @@ public class CameraGrabber : MonoBehaviour {
         
 
 	}
+
+
+    void OnGUI()
+    {
+
+        if (!display)
+        {
+            FillTexture(Color.red);
+            GUI.DrawTexture(new Rect(cursorPos.x, cursorPos.y, size, size), t);
+        }
+        else
+        {
+            FillTexture(Color.green);
+            GUI.DrawTexture(new Rect(cursorPos.x, cursorPos.y, size, size), t);
+        }
+    }
+    private void FillTexture(Color color)
+    {
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                t.SetPixel(x, y, color);
+            }
+        }
+        t.Apply();
+    }
+
 }
--- unityProject/Assets/Scripts/CursorColor.cs
+++ unityProject/Assets/Scripts/CursorColor.cs
@@ -3,68 +3,13 @@ using System.Collections;
 
 [RequireComponent(typeof(RayCast))]
 public class CursorColor : MonoBehaviour {
-	public static int size = 6;
-	public GameObject otherGameObject;
-    public Vector2 cursorPos;
-
-	private static Texture2D t;
-	public static bool display = false;
-    public bool dragOk = false;
-
-
-	private void Awake () {
-	    t = new Texture2D(size, size, TextureFormat.RGB24, true);
-		t.name = "Procedural Texture";
-		FillTexture(Color.green);
-        cursorPos = new Vector2(Screen.width / 2, Screen.height / 2 - 10);
-	}
-
-	public void OnMouseEnter () {
-        display = true;
-	}
-
-	public void OnMouseExit() {
-        if (!dragOk)
-        {
-            display = false;
-        }
-	}
-
-    void OnMouseDown() 
-	{
-		display = true;
-        dragOk = true;
-
-	}
 	
-	void OnMouseUp () 
-	{
-		display = false;
-        dragOk = false;
-        
-	}
+	public GameObject otherGameObject;
 
-	private void FillTexture (Color color) {
-		for (int y = 0; y < size; y++) {
-			for (int x = 0; x < size; x++) {
-				t.SetPixel(x, y, color);
-			}
-		}
-		t.Apply();
-	}
+	
 
-    void OnGUI()
-    {
-        
-        if (!display)
-        {
-            FillTexture(Color.red);
-            GUI.DrawTexture(new Rect(cursorPos.x, cursorPos.y , size, size), t);
-        }
-        else
-        {
-            FillTexture(Color.green);
-            GUI.DrawTexture(new Rect(cursorPos.x, cursorPos.y, size, size), t);
-        }
-    }
+	
+	
+
+    
 }
