0a6a1a4032d08eeba90c461ac4bc452b9a2f9f21
manumanmax <manua380@hotmail.fr>
2015-04-02 14:47:58 +0200

    whatever

--- unityProject/Assets/Scripts/CreateObjects.cs
+++ unityProject/Assets/Scripts/CreateObjects.cs
@@ -2,22 +2,7 @@
 using System.Collections.Generic;
 
 public class CreateObjects : MonoBehaviour {
-    /*public GameObject controller = new GameObject();
-    public GameObject cube_1 = new GameObject();
-    public GameObject cube_2 = new GameObject();
-    public GameObject cube_3 = new GameObject();
-    public GameObject storeArea = new GameObject();
-    public GameObject dropArea = new GameObject();
-    public GameObject resultArea = new GameObject();
-    public GameObject storePanel = new GameObject();
-    public GameObject dropPanel = new GameObject();
-    public GameObject resultPanel = new GameObject();
-    public GameObject lightGameObject;
-    public GameObject dropLight;
-    public Vector3 storeLightPos;
-    public Vector4 lightColor;
-    public float panelSpace;
-    */
+   
 
     void Awake()
     {
@@ -27,88 +12,7 @@ public class CreateObjects : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        //panelSpace = 10.6f;
-        /*
-        controller = new GameObject();
-        controller.AddComponent<CharacterController>();
-        controller.AddComponent<MouseLook>();
-        controller.AddComponent<Camera>();
-        controller.AddComponent<GUILayer>();
-        controller.AddComponent<CameraGrabber>();
-        controller.tag = "MainCamera";
-
-        controller.transform.position = new Vector3((float)(15 - (panelSpace)), 5, 10);
-        */
-        // cubes
-        /*
-        cube_1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube_1.transform.position = new Vector3((float)(15 - (0)), 1, -2);
-        cube_1.AddComponent<CursorColor>();
-        Rigidbody r = new Rigidbody();
-        cube_1.AddComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionZ; ;
-        cube_1.AddComponent<BoxCollider>();
-        cube_2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube_2.transform.position = new Vector3((float)(15 - (1.5)), 1, -2);
-        cube_2.AddComponent<CursorColor>(); 
-        cube_2.AddComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionZ;
-        cube_2.AddComponent<BoxCollider>();
-        cube_3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube_3.transform.position = new Vector3((float)(15 - (1.5)), 1, -2);
-        cube_3.AddComponent<CursorColor>();
-        cube_3.AddComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionZ; ;
-        cube_3.AddComponent<BoxCollider>();
-        cube_1.AddComponent<RayCast>();
-        cube_2.AddComponent<RayCast>();
-        cube_3.AddComponent<RayCast>();
-
-        */
-        // Areas
-        /*
-        storeArea = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        storeArea.name = "storeArea";
-        storeArea.transform.position = new Vector3((float)(15 - (0)), 1, -4);
-        dropArea = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        dropArea.transform.position = new Vector3((float)(15 - (panelSpace)), 1, -4);
-        dropArea.name = "dropArea";
-        resultArea = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        resultArea.transform.position = new Vector3((float)(15 - (2 * panelSpace)), 1, -4);
-        resultArea.name = "resultArea";
-        */
-        // Panels
-        /*
-        storePanel = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        storePanel.name = "storePanel";
-        storePanel.transform.position = new Vector3((float)(15 - 0), 1, -4);
-        storePanel.transform.position = storePanel.transform.position + new Vector3(0, 5, 0);
-        storePanel.transform.Rotate(new Vector3(90, 0, 0));
-        storePanel.transform.Translate(new Vector3(0, -4, -1));
-
-        dropPanel = (GameObject)Instantiate(storePanel);
-        dropPanel.transform.position = storePanel.transform.position + new Vector3(-(panelSpace), 0.0f, 0.0f);
-        dropPanel.name = "dropPanel";
-        resultPanel = (GameObject)Instantiate(dropPanel);
-        resultPanel.transform.position = storePanel.transform.position + new Vector3(-(2 * panelSpace), 0.0f, 0.0f);
-        resultPanel.name = "resultPanel";
-        */
-
-        //lights
-        /*
-        storeLightPos = new Vector3(15.2f, 30f, -3.224f);
-        lightColor = new Vector4(200, 172, 85, 255);
-        lightGameObject = new GameObject("storeLight");
-        lightGameObject.AddComponent<Light>();
-        lightGameObject.light.color = lightColor;
-        lightGameObject.light.type = LightType.Spot;
-        lightGameObject.transform.position = storeLightPos;
-        lightGameObject.light.intensity = 1;
-        lightGameObject.light.range = 30f;
-        lightGameObject.transform.Rotate(new Vector3(90, 0, 0));
-
-        dropLight = (GameObject)Instantiate(lightGameObject);
-        dropLight.transform.Translate(new Vector3(-panelSpace, 0, 0));
-        dropLight.name = "dropLight";*/
-
-        //Camera.main.transform.LookAt(dropPanel.transform);
+        
         
     }
 
--- unityProject/Assets/Scripts/CursorColor.cs
+++ unityProject/Assets/Scripts/CursorColor.cs
@@ -1,75 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 [RequireComponent(typeof(RayCast))]
-public class CursorColor : MonoBehaviour {
-	public static int size = 6;
-	public GameObject otherGameObject;
+public class CursorColor : MonoBehaviour
+{
+    private CameraGrabber cameraScript;
 
-	private static Texture2D t;
-	public static bool display = false;
     public bool dragOk = false;
 
 
-	private void Awake () {
-	    t = new Texture2D(size, size, TextureFormat.RGB24, true);
-		t.name = "Procedural Texture";
-		FillTexture(Color.green);
-        
-	}
-
-	public void OnMouseEnter () {
-        print("entred");
-        display = true;
-		//Cursor.SetCursor(t, Vector2.zero, CursorMode.ForceSoftware);
-	}
-
-	public void OnMouseExit() {
-        if (!dragOk)
-        {
-            //Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
-            display = false;
-        }
-	}
-
-    void OnMouseDown() 
-	{
-        print("clecked");   
-		display = true;
-        dragOk = true;
-
-	}
-	
-	void OnMouseUp () 
-	{
-        /*if(display)
-            Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);*/
-		display = false;
-        dragOk = false;
-        
-	}
-
-	private void FillTexture (Color color) {
-		for (int y = 0; y < size; y++) {
-			for (int x = 0; x < size; x++) {
-				t.SetPixel(x, y, color);
-			}
-		}
-		t.Apply();
-	}
-
-    void OnGUI()
+    private void Awake()
     {
-        
-        if (!display)
+        cameraScript = GetComponent<CameraGrabber>();
+    }
+
+    void OnMouseDown()
+    {
+        if (cameraScript.display)
         {
-            FillTexture(Color.red);
-            GUI.DrawTexture(new Rect(Screen.width / 2, Screen.height / 2 - 10 , size, size), t);
-        }
-        else
-        {
-            FillTexture(Color.green);
-            GUI.DrawTexture(new Rect(Screen.width / 2, Screen.height / 2 - 10 , size, size), t);
+            print("#cursorColor : draging is possible");
+            dragOk = true;
         }
     }
+
+    void OnMouseUp()
+    {
+        dragOk = false;
+
+    }
+
+
+
+
 }
