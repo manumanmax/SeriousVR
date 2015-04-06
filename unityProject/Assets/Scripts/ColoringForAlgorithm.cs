f1b19da3b346ed390f57befad64689f44e15d681
manumanmax <manua380@hotmail.fr>
2015-04-07 00:11:41 +0200

    move Materials and prefab into resources for load

--- unityProject/Assets/Scripts/ColoringForAlgorithm.cs
+++ unityProject/Assets/Scripts/ColoringForAlgorithm.cs
@@ -1,24 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ColoringForAlgorithm : MonoBehaviour {
+public class ColoringForAlgorithm : MonoBehaviour
+{
 
     private GameObject[] receivers;
     private bool receiversWellFilled;
+    private GameObject[] cubes;
+    public int numberOfCubes = 3;
     void Awake()
     {
         receivers = GameObject.FindGameObjectsWithTag("receiver");
+        apply(Color.red, 1, 3);
 
-        
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         foreach (GameObject go in receivers)
         {
             if (go.GetComponent<ReceiverScript>().validObject == null)
@@ -35,10 +40,34 @@ public class ColoringForAlgorithm : MonoBehaviour {
             foreach (GameObject go in receivers)
             {
                 GameObject lockedObject = go.GetComponent<ReceiverScript>().lockedObject;
-                if(lockedObject != null)
+                if (lockedObject != null)
                     Debug.Log(go.GetComponent<ReceiverScript>().lockedObject.name);
             }
         }
+    }
+    void apply(Color initColor, int increment, int numberOfTimes)
+    {
+        if (numberOfTimes != numberOfCubes)
+        {
+            //TODO: change the texture of the result panel to fail
+            return;
+        }
+        cubes = GameObject.FindGameObjectsWithTag("changinCube");
 
-	}
+        Material red = Instantiate(Resources.Load("Materials/red", typeof(Material))) as Material;
+        Debug.Log(red);
+
+        for (int i = 0; i < numberOfTimes; i += increment)
+        {
+            if (i < 0)
+            {
+                //TODO: change the texture of the result panel to fail
+            }
+            else
+            {
+                cubes[i].renderer.material = red;
+            }
+        }
+
+    }
 }
