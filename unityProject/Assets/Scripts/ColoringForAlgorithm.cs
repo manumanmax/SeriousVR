a5f4846cc7c45d82c3836892c829edad2d0428e7
manumanmax <manua380@hotmail.fr>
2015-04-07 01:02:28 +0200

    add getColor from string and getIncrement

--- unityProject/Assets/Scripts/ColoringForAlgorithm.cs
+++ unityProject/Assets/Scripts/ColoringForAlgorithm.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class ColoringForAlgorithm : MonoBehaviour
 {
+    //algorithm final values
+    Color color;
+    int increment;
+    int startValue;
+
 
     private GameObject[] receivers;
     private bool receiversWellFilled;
@@ -11,6 +16,7 @@ public class ColoringForAlgorithm : MonoBehaviour
     void Awake()
     {
         receivers = GameObject.FindGameObjectsWithTag("receiver");
+
         apply(Color.red, 1, 3);
 
     }
@@ -41,10 +47,74 @@ public class ColoringForAlgorithm : MonoBehaviour
             {
                 GameObject lockedObject = go.GetComponent<ReceiverScript>().lockedObject;
                 if (lockedObject != null)
-                    Debug.Log(go.GetComponent<ReceiverScript>().lockedObject.name);
+                {
+                    parse(go);
+                }
+
             }
         }
     }
+
+    private void parse(GameObject go)
+    {
+        // type
+        switch (go.GetComponent<Proprety>().name)
+        {
+            case "initialiser":
+                color = getColor(go.name);
+                break;
+            case "increment":
+                increment = getIncrement(go.name);
+                break;
+        }
+
+
+    }
+
+    private Color getColor(string name)
+    {
+        switch (name)
+        {
+            case "green":
+                return Color.green;
+            case "blue":
+                return Color.blue;
+            case "grey":
+                return Color.grey;
+            case "yellow":
+                return Color.yellow;
+            case "red":
+                return Color.red;
+            default:
+                return Color.white;
+        }
+    }
+
+    private int getIncrement(string name)
+    {
+        switch (name)
+        {
+            case "i++":
+                return 1;
+            case "i--":
+                return -1;
+            default:
+                char[] charName = name.ToCharArray();
+                if (charName[1] == '-')
+                {
+                    return -(int.Parse(name.Substring(2, -1)));
+                }
+                else if (charName[1] == '+')
+                {
+                    return int.Parse(name.Substring(2, -1));
+                }
+                else
+                {
+                    return 1;
+                }
+        }
+    }
+
     void apply(Color initColor, int increment, int numberOfTimes)
     {
         if (numberOfTimes != numberOfCubes)
@@ -53,9 +123,14 @@ public class ColoringForAlgorithm : MonoBehaviour
             return;
         }
         cubes = GameObject.FindGameObjectsWithTag("changinCube");
-
-        Material red = Instantiate(Resources.Load("Materials/red", typeof(Material))) as Material;
-        Debug.Log(red);
+        GameObject tmp = cubes[0];
+        cubes[0] = cubes[2];
+        cubes[2] = tmp;
+        Material[] materials = new Material[numberOfCubes];
+        materials[0] = (Instantiate(Resources.Load("Materials/red", typeof(Material))) as Material);
+        materials[1] = (Instantiate(Resources.Load("Materials/green", typeof(Material))) as Material);
+        materials[2] = (Instantiate(Resources.Load("Materials/blue", typeof(Material))) as Material);
+        Debug.Log(materials);
 
         for (int i = 0; i < numberOfTimes; i += increment)
         {
@@ -65,7 +140,7 @@ public class ColoringForAlgorithm : MonoBehaviour
             }
             else
             {
-                cubes[i].renderer.material = red;
+                cubes[i].renderer.material = materials[i];
             }
         }
 
